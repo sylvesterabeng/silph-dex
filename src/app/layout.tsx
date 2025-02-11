1c3c0cc6730b3890363a181d472635d4b428482c
sylvesterabeng <sylvesterabeng94@gmail.com>
2025-02-11 21:08:33 +0900

    Added speed insight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { Theme } from '@radix-ui/themes'
 import { Analytics } from '@vercel/analytics/next'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import { Protest_Guerrilla, Ubuntu_Sans } from 'next/font/google'
 import React from 'react'
 
@@ -70,6 +71,7 @@ export default function RootLayout({
           {children}
         </Theme>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
